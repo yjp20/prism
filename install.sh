7169f5caa51a1f26c778017b9a858a6df072e734
Tom Pytleski <pytlesk4@gmail.com>
2017-03-01 18:06:09 -0600

    - clean up messages

--- install.sh
+++ install.sh
@@ -27,16 +27,14 @@ fi
 
 LATEST=$(curl -s https://api.github.com/repos/stoplightio/prism/tags | grep -Eo '"name":.*?[^\\]",'  | head -n 1 | sed 's/[," ]//g' | cut -d ':' -f 2)
 URL="https://github.com/stoplightio/prism/releases/download/$LATEST/prism_$PLATFORM"
-SRC=prism_$PLATFORM
+SRC=$(pwd)/prism_$PLATFORM
 DEST=/usr/local/bin/prism
 
 if [ -z $LATEST ] ; then
   echo "Error requesting. Download binary from ${URL}"
   exit 1
 else
-  STATUS=$(curl -sL -w %{http_code} -O $URL)
-  echo "$SRC"
-  echo "$STATUS"
+  STATUS=$(curl -sL -w %{http_code} -o $SRC $URL)
   if [ $STATUS -ge 200 ] & [ $STATUS -le 308 ]; then
     mv $SRC $DEST
     chmod +x $DEST
