a9ca25d3c3782e07619f41fc0c0c1622654072bc
Tom Pytleski <pytlesk4@gmail.com>
2017-04-24 09:49:12 -0500

    - don’t install alpha version of prism.

--- install.sh
+++ install.sh
@@ -25,8 +25,8 @@ elif [ "$UNAME" = "Linux" ] ; then
   fi
 fi
 
-LATEST=$(curl -s https://api.github.com/repos/stoplightio/prism/tags | grep -Eo '"name":.*?[^\\]",'  | head -n 1 | sed 's/[," ]//g' | cut -d ':' -f 2)
-URL="https://github.com/stoplightio/prism/releases/download/$LATEST/prism_$PLATFORM"
+# LATEST=$(curl -s https://api.github.com/repos/stoplightio/prism/tags | grep -Eo '"name":.*?[^\\]",'  | head -n 1 | sed 's/[," ]//g' | cut -d ':' -f 2)
+URL="https://github.com/stoplightio/prism/releases/download/v0.6.21/prism_$PLATFORM"
 SRC=$(pwd)/prism_$PLATFORM
 DEST=/usr/local/bin/prism
 
