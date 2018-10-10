58e254a5544118ac49323ef72961f70e6de29b15
Karol Maciaszek <karol@spotlight.io>
2018-10-10 17:27:07 +0200

    More tests of HttpProvider

--- src/mocker/http/HttpProvider.spec.ts
+++ src/mocker/http/HttpProvider.spec.ts
@@ -11,7 +11,7 @@ describe('HttpProvider', () => {
   });
 
   describe('mock()', () => {
-    it('mocks', async () => {
+    it('mocks correctly for given status/contentType/example', async () => {
       const operation: IHttpOperation = {
         id: 'id',
         method: 'GET',
@@ -38,6 +38,78 @@ describe('HttpProvider', () => {
       };
 
       expect(await httpProvider.mock(operation, options)).toMatchSnapshot();
+    });
+
+    it('fails when status code is missing', async () => {
+      const operation: IHttpOperation = {
+        id: 'id',
+        method: 'GET',
+        path: '/test',
+        responses: [],
+      };
+
+      const options: IHttpOperationOptions = {
+        protocol: Protocol.HTTP,
+        dynamic: false,
+        status: '200',
+        contentType: 'application/json',
+        example: 'example1',
+      };
+
+      expect(httpProvider.mock(operation, options)).rejects.toThrowErrorMatchingSnapshot();
+    });
+
+    it('fails when media type is missing', async () => {
+      const operation: IHttpOperation = {
+        id: 'id',
+        method: 'GET',
+        path: '/test',
+        responses: [
+          {
+            code: '200',
+            content: [],
+          },
+        ],
+      };
+
+      const options: IHttpOperationOptions = {
+        protocol: Protocol.HTTP,
+        dynamic: false,
+        status: '200',
+        contentType: 'application/json',
+        example: 'example1',
+      };
+
+      expect(httpProvider.mock(operation, options)).rejects.toThrowErrorMatchingSnapshot();
+    });
+
+    it('fails when example is missing', async () => {
+      const operation: IHttpOperation = {
+        id: 'id',
+        method: 'GET',
+        path: '/test',
+        responses: [
+          {
+            code: '200',
+            content: [
+              {
+                mediaType: 'application/json',
+                examples: [],
+              },
+            ],
+          },
+        ],
+      };
+
+      const options: IHttpOperationOptions = {
+        protocol: Protocol.HTTP,
+        dynamic: false,
+        status: '200',
+        contentType: 'application/json',
+        example: 'example1',
+      };
+
+      expect(httpProvider.mock(operation, options)).rejects.toThrowErrorMatchingSnapshot();
     });
   });
 });
