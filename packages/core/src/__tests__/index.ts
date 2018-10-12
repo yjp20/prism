8e3272b6dc74c16cae4839d74e4686704cce0d59
Marc MacLeod <marbemac@gmail.com>
2018-10-11 22:11:41 -0500

    clarification comment

--- packages/core/src/__tests__/index.ts
+++ packages/core/src/__tests__/index.ts
@@ -7,6 +7,7 @@ describe('graph', () => {
       let customLoaderId = 0;
 
       const createInstance = factory<any, any, any, any, { id: number }>({
+        // the default loader, what implementations of prism define (prism-http, etc)
         loader: {
           load: async opts => {
             defaultLoaderId = opts && opts.id ? opts.id : 0;
@@ -15,13 +16,20 @@ describe('graph', () => {
         },
       });
 
+      /**
+       * the user imports createInstance from an implementation (like prism-http)
+       * when creating an instance, the user can override any of the components, and receive the default component
+       * as an argument
+       */
       const prism = createInstance({
         loader: {
           load: async (opts, defaultLoader) => {
-            // end user can call, or not call, the parent loader
-            // calling it will invoke the default load function above, setting defaultLoaderId
-            // this allows one to hook before and after components functions are run,
-            // and add their own custom logic, change the options passed through, or skip altogether
+            /**
+             * end user can call, or not call, the parent loader
+             * calling it will invoke the default load function above, setting defaultLoaderId
+             * this allows one to hook before and after components functions are run,
+             * and add their own custom logic, change the options passed through, or skip altogether
+             */
             if (defaultLoader) {
               await defaultLoader.load(opts);
             }
