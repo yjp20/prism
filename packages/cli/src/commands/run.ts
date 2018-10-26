2a10a7d26025616b306870e1d3b9f4b7e1b95bb7
Chris Miaskowski <chris.miaskowski@tenfold.com>
2018-10-26 14:11:28 +0200

    SL-84 clean up run command

--- packages/cli/src/commands/run.ts
+++ packages/cli/src/commands/run.ts
@@ -3,24 +3,6 @@ import { TPrismHttpComponents } from '@stoplight/prism-http';
 import { createServer } from '@stoplight/prism-http-server';
 import resources from '../resources';
 
-// NAME:
-//    prism run - Start a server with the given spec file.
-
-// USAGE:
-//    prism run [command options] [arguments...]
-
-// OPTIONS:
-//    --config, -c value    File path to a Prism config.json file.
-//    --spec, -s value      File path or URL to swagger 2 spec.
-//    --port, -p value      Port that Prism will run on, defaults to 4010. (default: 4010)
-//    --mock, -m                  Turn global mocking on or off, defaults to off.
-//    --mockDynamic, --md         Turn global dynamic mocking on or off, defaults to off.
-//    --list, -l                  Pretty print endpoints path + method at start.
-//    --validate, --vl            Validate the passed in spec at start.
-//    --debug, -d                 Turn debug on or off, defaults to off.
-//    --cors, --co                Turn CORS on, defaults to off.
-//    --variable, -v value  Overwrite enviroment variables, key=value.
-
 export default class Run extends Command {
   public static description = 'Start a server with the given spec file';
   public static flags = {
@@ -43,9 +25,9 @@ export default class Run extends Command {
 
   public async run() {
     const { flags } = this.parse(Run);
-    const path = '';
-    const { port, mock } = flags;
+    const { port, spec, mock } = flags;
     const components: TPrismHttpComponents<any> = {
+      // TODO: remove once validator implemented
       validator: undefined,
       // TODO: remove once loader implemented
       loader: {
@@ -59,7 +41,7 @@ export default class Run extends Command {
         mock: false,
       };
     }
-    const server = createServer({ path }, { components });
+    const server = createServer({ path: spec }, { components });
     server
       .listen(port as number)
       .then(console.log)
