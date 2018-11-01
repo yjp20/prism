8d271edf0e0168c8992d27caa634149e0df17433
Karol Maciaszek <karol@maciaszek.pl>
2018-11-01 14:21:35 +0100

    SL-81 Code style fix

--- packages/http/src/validator/helpers/__tests__/findResponseSpec.spec.ts
+++ packages/http/src/validator/helpers/__tests__/findResponseSpec.spec.ts
@@ -4,7 +4,11 @@ describe('findResponseSpec()', () => {
   it('works', () => {
     expect(
       findResponseSpec(
-        [{ code: '2XX', contents: [] }, { code: '20X', contents: [] }, { code: '1XX', contents: [] }],
+        [
+          { code: '2XX', contents: [] },
+          { code: '20X', contents: [] },
+          { code: '1XX', contents: [] },
+        ],
         200
       )
     ).toEqual({ code: '20X', contents: [] });
