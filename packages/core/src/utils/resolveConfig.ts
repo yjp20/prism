ddcbb83f821e3a2fabf010025bce3893f1580bce
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-03-21 12:48:23 +0100

    refactor: use refinements to avoid casts

--- packages/core/src/utils/resolveConfig.ts
+++ packages/core/src/utils/resolveConfig.ts
@@ -1,13 +1,17 @@
 import { PrismConfig, PrismConfigFactory } from '../types';
 
+function isPrismConfigFactory<C, I>(val: PrismConfig<C, I>): val is PrismConfigFactory<C, I> {
+  return typeof val === 'function';
+}
+
 export function resolveConfig<Config, Input>(
   input: Input,
   config: PrismConfig<Config, Input>,
   defaultConfig?: PrismConfig<Config, Input>
 ): Config {
-  if (typeof config === 'function') {
-    // config factory function
-    return (config as PrismConfigFactory<Config, Input>)(input, defaultConfig);
+  if (isPrismConfigFactory<Config, Input>(config)) {
+    return config(input, defaultConfig);
   }
+
   return config;
 }
