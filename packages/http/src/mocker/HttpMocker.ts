dbf13355b65ce03bf24731229bcb85664a5f3310
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-03-21 13:04:51 +0100

    refactor: get rid of casts

--- packages/http/src/mocker/HttpMocker.ts
+++ packages/http/src/mocker/HttpMocker.ts
@@ -1,5 +1,5 @@
 import { IMocker, IMockerOpts } from '@stoplight/prism-core';
-import { IHttpOperation, INodeExample } from '@stoplight/types';
+import { IHttpOperation } from '@stoplight/types';
 
 import { IHttpConfig, IHttpRequest, IHttpResponse } from '../types';
 import { getHeaderByName } from '../validator/utils/http';
@@ -56,8 +56,9 @@ Here is the original validation result instead: ${JSON.stringify(input.validatio
 
     // preparing response body
     let body;
-    const example = negotiationResult.example as INodeExample;
-    if (example && example.value !== undefined) {
+    const example = negotiationResult.example;
+
+    if (example && 'value' in example && example.value !== undefined) {
       body = example.value;
     } else if (negotiationResult.schema) {
       body = await this._exampleGenerator.generate(
