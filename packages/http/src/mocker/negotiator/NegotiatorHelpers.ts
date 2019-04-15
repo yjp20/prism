fc03e90715dbacf00224d3ffb8b09f776607c347
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-04-15 12:22:32 +0200

    feat: return and transform the default response if defined

--- packages/http/src/mocker/negotiator/NegotiatorHelpers.ts
+++ packages/http/src/mocker/negotiator/NegotiatorHelpers.ts
@@ -34,7 +34,11 @@ function findResponseByStatusCode(
   responses: IHttpOperationResponse[],
   statusCode: string
 ): IHttpOperationResponse | undefined {
-  return responses.find(response => response.code.toLowerCase() === statusCode.toLowerCase());
+  const candidate = responses.find(response => response.code.toLowerCase() === statusCode.toLowerCase());
+  if (candidate) {
+    return candidate;
+  }
+  return Object.assign(responses.find(response => response.code === 'default'), { code: statusCode });
 }
 
 const helpers = {
