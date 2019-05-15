bcdaffc607154a7cbfd09ff54ce8db0fd94a495b
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-05-15 18:26:25 -0500

    test: fix

--- packages/core/src/utils/__tests__/graphFacade.int.ts
+++ packages/core/src/utils/__tests__/graphFacade.int.ts
@@ -24,10 +24,11 @@ describe('graphFacade', () => {
 
   describe('createRawNode()', () => {
     test('httpOperations should return filtered nodes', async () => {
-      await graphFacade.createRawNode(
-        JSON.stringify(require('../../../../cli/src/samples/no-refs-petstore.oas2.json')),
-        { type: FilesystemNodeType.File, language: 'json' },
-      );
+      const path = '../../../../http/src/__tests__/fixtures/no-refs-petstore.oas2.json';
+      await graphFacade.createRawNode(JSON.stringify(require(path)), {
+        type: FilesystemNodeType.File,
+        path: require.resolve(path),
+      });
 
       expect(graphFacade.httpOperations.length).toBeGreaterThan(0);
     });
