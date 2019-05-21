0e6038b125ba0ad7343536086089331b01d68d9e
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-05-21 15:13:21 -0500

    chore: compilation issues

--- packages/http/src/mocker/negotiator/types.ts
+++ packages/http/src/mocker/negotiator/types.ts
@@ -1,5 +1,5 @@
-import { INodeExample, INodeExternalExample, ISchema } from '@stoplight/types';
 import { IHttpOperationConfig } from '@stoplight/prism-http';
+import { INodeExample, INodeExternalExample, ISchema } from '@stoplight/types';
 
 export interface IHttpNegotiationResult {
   code: string;
