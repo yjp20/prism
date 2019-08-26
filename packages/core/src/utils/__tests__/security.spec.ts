668c9039ab7bf7446d6db55f3745ee18d170e855
lag-of-death <mateuszwit21@gmail.com>
2019-08-26 18:05:39 +0200

    fix: Handle `and relation` between security schemes (#546)
    * fix: handle and relation between security schemes

--- packages/core/src/utils/__tests__/security.spec.ts
+++ packages/core/src/utils/__tests__/security.spec.ts
@@ -3,22 +3,25 @@ import { validateSecurity } from '../security';
 import { assertNone, assertSome } from './utils';
 
 describe('validateSecurity', () => {
+  const token = new Buffer('test:test').toString('base64');
+
   it('passes the validation', () => {
-    assertNone(validateSecurity({}, { security: [] }));
+    assertNone(validateSecurity({}, { security: [[]] }));
   });
 
   it('fails with a message explaining the issue', () => {
-    assertSome(validateSecurity({}, { security: [{}] }), obj =>
-      expect(obj).toStrictEqual(new Error('We currently do not support this type of security scheme.')),
+    assertSome(validateSecurity({}, { security: [[{}]] }), obj =>
+      expect(obj).toStrictEqual({
+        message: 'We currently do not support this type of security scheme.',
+        severity: DiagnosticSeverity.Warning,
+      }),
     );
   });
 
   describe('when security scheme uses Basic authorization', () => {
-    const securityScheme = [{ scheme: 'basic', type: 'http' }];
+    const securityScheme = [[{ scheme: 'basic', type: 'http' }]];
 
     it('passes the validation', () => {
-      const token = new Buffer('test:test').toString('base64');
-
       assertNone(validateSecurity({ headers: { authorization: `Basic ${token}` } }, { security: securityScheme }));
     });
 
@@ -45,7 +48,7 @@ describe('validateSecurity', () => {
   });
 
   describe('when security scheme uses Digest authorization', () => {
-    const securityScheme = [{ scheme: 'digest', type: 'http' }];
+    const securityScheme = [[{ scheme: 'digest', type: 'http' }]];
 
     it('passes the validation', () => {
       assertNone(
@@ -70,7 +73,7 @@ describe('validateSecurity', () => {
   });
 
   describe('when security scheme uses Bearer authorization', () => {
-    const securityScheme = [{ scheme: 'bearer', type: 'http' }];
+    const securityScheme = [[{ scheme: 'bearer', type: 'http' }]];
 
     it('passes the validation', () => {
       assertNone(validateSecurity({ headers: { authorization: 'Bearer abc123' } }, { security: securityScheme }));
@@ -100,7 +103,7 @@ describe('validateSecurity', () => {
   });
 
   describe('when security scheme uses OAuth2 authorization', () => {
-    const securityScheme = [{ type: 'oauth2' }];
+    const securityScheme = [[{ type: 'oauth2' }]];
 
     it('it passes the validation', () => {
       assertNone(validateSecurity({ headers: { authorization: 'Bearer abc123' } }, { security: securityScheme }));
@@ -119,7 +122,7 @@ describe('validateSecurity', () => {
   });
 
   describe('when security scheme uses OpenID authorization', () => {
-    const securityScheme = [{ type: 'openIdConnect' }];
+    const securityScheme = [[{ type: 'openIdConnect' }]];
 
     it('passes the validation', () => {
       assertNone(validateSecurity({ headers: { authorization: 'Bearer abc123' } }, { security: securityScheme }));
@@ -142,9 +145,9 @@ describe('validateSecurity', () => {
       it('does not add info to WWW-Authenticate header', () => {
         assertSome(
           validateSecurity(
-            { tags: [] },
+            { headers: {} },
             {
-              security: [{ scheme: 'basic', type: 'http' }, { in: 'header', type: 'apiKey', name: 'x-api-key' }],
+              security: [[{ scheme: 'basic', type: 'http' }, { in: 'header', type: 'apiKey', name: 'x-api-key' }]],
             },
           ),
           res =>
@@ -159,14 +162,14 @@ describe('validateSecurity', () => {
     });
 
     describe('when api key is expected to be found in a header', () => {
-      const securityScheme = [{ in: 'header', type: 'apiKey', name: 'x-api-key' }];
+      const securityScheme = [[{ in: 'header', type: 'apiKey', name: 'x-api-key' }]];
 
       it('passes the validation', () => {
         assertNone(validateSecurity({ headers: { 'x-api-key': 'abc123' } }, { security: securityScheme }));
       });
 
       it('fails with an invalid security scheme error', () => {
-        assertSome(validateSecurity({ tags: [] }, { security: securityScheme }), res =>
+        assertSome(validateSecurity({ headers: {} }, { security: securityScheme }), res =>
           expect(res).toStrictEqual({
             code: 401,
             tags: [],
@@ -178,7 +181,7 @@ describe('validateSecurity', () => {
     });
 
     describe('when api key is expected to be found in the query', () => {
-      const securityScheme = [{ in: 'query', type: 'apiKey', name: 'key' }];
+      const securityScheme = [[{ in: 'query', type: 'apiKey', name: 'key' }]];
 
       it('passes the validation', () => {
         assertNone(validateSecurity({ url: { query: { key: 'abc123' } } }, { security: securityScheme }));
@@ -197,7 +200,7 @@ describe('validateSecurity', () => {
     });
 
     describe('when api key is expected to be found in a cookie', () => {
-      const securityScheme = [{ in: 'cookie', type: 'apiKey', name: 'key' }];
+      const securityScheme = [[{ in: 'cookie', type: 'apiKey', name: 'key' }]];
 
       it('passes the validation', () => {
         assertNone(validateSecurity({ headers: { cookie: 'key=abc123' } }, { security: securityScheme }));
@@ -211,6 +214,168 @@ describe('validateSecurity', () => {
             message: 'Invalid security scheme used',
             severity: DiagnosticSeverity.Error,
           }),
+        );
+      });
+    });
+  });
+
+  describe('OR relation between security schemes', () => {
+    const securityScheme = [[{ scheme: 'bearer', type: 'http' }], [{ scheme: 'basic', type: 'http' }]];
+
+    it('fails with an invalid security scheme error', () => {
+      assertSome(
+        validateSecurity(
+          {},
+          {
+            security: securityScheme,
+          },
+        ),
+        res =>
+          expect(res).toStrictEqual({
+            code: 401,
+            message: 'Invalid security scheme used',
+            severity: DiagnosticSeverity.Error,
+            tags: ['Bearer', 'Basic realm="*"'],
+          }),
+      );
+    });
+
+    it('passes the validation', () => {
+      assertNone(
+        validateSecurity(
+          { headers: { authorization: 'Bearer abc123' } },
+          {
+            security: securityScheme,
+          },
+        ),
+      );
+    });
+
+    it('passes the validation', () => {
+      assertNone(
+        validateSecurity(
+          { headers: { authorization: `Basic ${token}` } },
+          {
+            security: securityScheme,
+          },
+        ),
+      );
+    });
+  });
+
+  describe('AND relation between security schemes', () => {
+    const headerScheme = {
+      in: 'header',
+      type: 'apiKey',
+      name: 'x-api-key',
+    };
+
+    describe('when 2 different security schemes are expected', () => {
+      describe('expecting oauth + apikey', () => {
+        const securityScheme = [[headerScheme, { type: 'oauth2' }]];
+
+        it('fails with an invalid security scheme error', () => {
+          assertSome(
+            validateSecurity(
+              { headers: { 'x-api-key': 'abc123' } },
+              {
+                security: securityScheme,
+              },
+            ),
+            res =>
+              expect(res).toStrictEqual({
+                code: 401,
+                message: 'Invalid security scheme used',
+                severity: DiagnosticSeverity.Error,
+                tags: ['OAuth2'],
+              }),
+          );
+        });
+
+        it('passes the validation', () => {
+          assertNone(
+            validateSecurity(
+              { headers: { 'x-api-key': 'abc123', authorization: 'Bearer abc123' } },
+              {
+                security: securityScheme,
+              },
+            ),
+          );
+        });
+      });
+
+      describe('expecting openid + apikey', () => {
+        const securityScheme = [[headerScheme, { type: 'openIdConnect' }]];
+
+        it('fails with an invalid security scheme error', () => {
+          assertSome(
+            validateSecurity(
+              { headers: { 'x-api-key': 'abc123' } },
+              {
+                security: securityScheme,
+              },
+            ),
+            res =>
+              expect(res).toStrictEqual({
+                code: 401,
+                message: 'Invalid security scheme used',
+                severity: DiagnosticSeverity.Error,
+                tags: ['OpenID'],
+              }),
+          );
+        });
+
+        it('passes the validation', () => {
+          assertNone(
+            validateSecurity(
+              { headers: { 'x-api-key': 'abc123', authorization: 'Bearer abc123' } },
+              {
+                security: securityScheme,
+              },
+            ),
+          );
+        });
+      });
+    });
+
+    describe('when security scheme expects two keys', () => {
+      const securityScheme = [
+        [
+          headerScheme,
+          {
+            in: 'query',
+            type: 'apiKey',
+            name: 'apiKey',
+          },
+        ],
+      ];
+
+      it('fails with an invalid security scheme error', () => {
+        assertSome(
+          validateSecurity(
+            { headers: { 'x-api-key': 'abc123' } },
+            {
+              security: securityScheme,
+            },
+          ),
+          res =>
+            expect(res).toStrictEqual({
+              code: 401,
+              message: 'Invalid security scheme used',
+              severity: DiagnosticSeverity.Error,
+              tags: [],
+            }),
+        );
+      });
+
+      it('passes the validation', () => {
+        assertNone(
+          validateSecurity(
+            { headers: { 'x-api-key': 'abc123' }, url: { query: { apiKey: 'abc123' } } },
+            {
+              security: securityScheme,
+            },
+          ),
         );
       });
     });
--- packages/core/src/utils/security/index.ts
+++ packages/core/src/utils/security/index.ts
@@ -1,36 +1,103 @@
-import { fold, isLeft, isRight, Left, left, mapLeft } from 'fp-ts/lib/Either';
+import { DiagnosticSeverity } from '@stoplight/types';
+import { Either, fold, isLeft, isRight, Left, left } from 'fp-ts/lib/Either';
 import { none, Option, some } from 'fp-ts/lib/Option';
 import { pipe } from 'fp-ts/lib/pipeable';
-import { get, identity } from 'lodash';
+import { flatten, get, identity } from 'lodash';
+import { noop, set } from 'lodash/fp';
 import { IPrismDiagnostic } from '../../types';
 import { securitySchemeHandlers } from './handlers';
 import { SecurityScheme } from './handlers/types';
 
-function getAllInvalidSec(invalidSecuritySchemes: Array<Left<IPrismDiagnostic>>): IPrismDiagnostic {
+function gatherInvalidResults(
+  error: Left<IPrismDiagnostic>,
+  invalidSecuritySchemes: Array<Array<Either<IPrismDiagnostic, unknown>>>,
+) {
   const firstLeftValue = pipe(
-    invalidSecuritySchemes[0],
+    error,
     fold<IPrismDiagnostic, unknown, IPrismDiagnostic>(identity, identity),
   );
 
-  if (firstLeftValue.code !== 401 || invalidSecuritySchemes.length === 1) {
-    return firstLeftValue;
-  } else {
-    const allWWWAuthHeaders = invalidSecuritySchemes.reduce((accumulator, currentValue) => {
-      return pipe(
-        currentValue,
-        mapLeft(err => [accumulator, get(err, ['headers', 'WWW-Authenticate'])].filter(identity).join(', ')),
-        fold(authHeader => authHeader || '', () => ''),
-      );
-    }, '');
+  const invalidSecurity =
+    firstLeftValue.code !== 401
+      ? firstLeftValue
+      : gatherWWWAuthHeader(invalidSecuritySchemes, ['tags'], firstLeftValue);
 
-    if (allWWWAuthHeaders !== '') {
-      return firstLeftValue.tags
-        ? Object.assign({}, firstLeftValue, { tags: firstLeftValue.tags.concat(allWWWAuthHeaders) })
-        : Object.assign({}, firstLeftValue, { tags: [allWWWAuthHeaders] });
-    } else {
-      return firstLeftValue;
-    }
+  return some(invalidSecurity);
+}
+
+function gatherValidationResults(securitySchemes: SecurityScheme[][], someInput: unknown, resource: unknown) {
+  const authResults = getAuthResults(securitySchemes, someInput, resource);
+
+  const validSecurityScheme = authResults.find(authRes => authRes.every(isRight));
+  const invalidSecuritySchemes = authResults.filter(authRes => authRes.some(isLeft));
+
+  const firstLeft = invalidSecuritySchemes[0] && invalidSecuritySchemes[0].find(isLeft);
+
+  if (!validSecurityScheme && firstLeft) {
+    return gatherInvalidResults(firstLeft, invalidSecuritySchemes);
+  } else {
+    return none;
   }
+}
+
+function gatherWWWAuthHeader(
+  authResults: Array<Array<Either<IPrismDiagnostic, unknown>>>,
+  pathToHeader: string[],
+  firstAuthErr: IPrismDiagnostic,
+) {
+  const flattenedAuthResults = flatten(authResults);
+
+  if (flattenedAuthResults.length === 1) {
+    return firstAuthErr;
+  } else {
+    const wwwAuthenticateHeaders = flattenedAuthResults.map(authResult => {
+      return pipe(
+        authResult,
+        fold(result => result.tags || [], noop),
+      );
+    });
+
+    const firstAuthErrWithAuthHeader = set(pathToHeader, flatten(wwwAuthenticateHeaders), firstAuthErr);
+
+    return wwwAuthenticateHeaders.every(identity) ? firstAuthErrWithAuthHeader : firstAuthErr;
+  }
+}
+
+function getAuthResult(
+  firstAuthErrAsLeft: Left<IPrismDiagnostic>,
+  authResult: Array<Either<IPrismDiagnostic, unknown>>,
+) {
+  const firstAuthErr: IPrismDiagnostic = pipe(
+    firstAuthErrAsLeft,
+    fold<IPrismDiagnostic, IPrismDiagnostic, IPrismDiagnostic>(identity, identity),
+  );
+
+  const invalidResultWithAuthHeader = gatherWWWAuthHeader([authResult], ['tags'], firstAuthErr);
+
+  return [left(invalidResultWithAuthHeader)];
+}
+
+function getAuthResults(securitySchemes: SecurityScheme[][], someInput: unknown, resource: unknown) {
+  return securitySchemes.map(securitySchemePairs => {
+    const authResult = securitySchemePairs.map(securityScheme => {
+      const schemeHandler = securitySchemeHandlers.find(handler => handler.test(securityScheme));
+
+      return schemeHandler
+        ? schemeHandler.handle(someInput, securityScheme.name, resource)
+        : left({
+            message: 'We currently do not support this type of security scheme.',
+            severity: DiagnosticSeverity.Warning,
+          });
+    });
+
+    const firstAuthErrAsLeft = authResult.find(isLeft);
+
+    if (firstAuthErrAsLeft) {
+      return getAuthResult(firstAuthErrAsLeft, authResult);
+    } else {
+      return authResult;
+    }
+  });
 }
 
 export function validateSecurity(someInput: unknown, resource?: unknown): Option<IPrismDiagnostic> {
@@ -38,18 +105,7 @@ export function validateSecurity(someInput: unknown, resource?: unknown): Option
 
   if (!securitySchemes.length) {
     return none;
+  } else {
+    return gatherValidationResults(securitySchemes, someInput, resource);
   }
-
-  const validatedSecuritySchemes = securitySchemes.map((definedSecScheme: SecurityScheme) => {
-    const schemeHandler = securitySchemeHandlers.find(handler => handler.test(definedSecScheme));
-
-    return schemeHandler
-      ? schemeHandler.handle(someInput, definedSecScheme.name, resource)
-      : left(new Error('We currently do not support this type of security scheme.'));
-  });
-
-  const validSecuritySchema = validatedSecuritySchemes.find(isRight);
-  const invalidSecuritySchemes = validatedSecuritySchemes.filter(isLeft);
-
-  return validSecuritySchema ? none : some(getAllInvalidSec(invalidSecuritySchemes));
 }
