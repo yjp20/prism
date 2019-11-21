a9eca6ecf29f619e23262a39851358124b248f5c
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-11-21 15:30:26 +0100

    refactor: make security validations easier to reason about (#830)
    * refactor: revert the conditions so we can use typeguards
    
    * refactor: step 1: get rid of array of arrays and functions not really useful
    
    * refactor: rename function names
    
    * filter on the caller side
    
    * refactor: do not ask a parameter we can grab from another
    
    * refactor: put the same parameter implict
    
    * refactor: avoid to use isLeft
    
    * refactor: remove another absurd function
    
    * refactor: other craziness removal
    
    * refactor: remove useless mapLeft
    
    * refactor:simplify
    
    * refactor: use option to check security provided or not
    
    * Update packages/http/src/validator/validators/security/index.ts
    
    Co-Authored-By: Karol Maciaszek <karol@maciaszek.pl>

--- packages/http/src/validator/validators/security/index.ts
+++ packages/http/src/validator/validators/security/index.ts
@@ -2,109 +2,54 @@ import { IHttpOperation, HttpSecurityScheme } from '@stoplight/types';
 import * as Either from 'fp-ts/lib/Either';
 import * as Option from 'fp-ts/lib/Option';
 import { pipe } from 'fp-ts/lib/pipeable';
-import { flatten, identity } from 'lodash';
-import { noop, set } from 'lodash/fp';
+import { flatten } from 'lodash';
+import { set } from 'lodash/fp';
 import { findSecurityHandler } from './handlers';
-import { NonEmptyArray } from 'fp-ts/lib/NonEmptyArray';
+import { NonEmptyArray, getSemigroup } from 'fp-ts/lib/NonEmptyArray';
+import { isNonEmpty, array } from 'fp-ts/lib/Array';
 import { IPrismDiagnostic, ValidatorFn } from '@stoplight/prism-core';
 import { IHttpRequest } from '../../../types';
 
-function gatherInvalidResults(
-  error: Either.Left<IPrismDiagnostic>,
-  invalidSecuritySchemes: Array<Array<Either.Either<IPrismDiagnostic, unknown>>>
-) {
-  const invalidSecurity = gatherWWWAuthHeader(invalidSecuritySchemes, ['tags'], error.left);
-  return Option.some(invalidSecurity);
+type HeadersAndUrl = Pick<IHttpRequest, 'headers' | 'url'>;
+
+const EitherValidation = Either.getValidation(getSemigroup<IPrismDiagnostic>());
+const eitherSequence = array.sequence(EitherValidation);
+
+function getValidationResults(securitySchemes: HttpSecurityScheme[][], input: HeadersAndUrl) {
+  const [first, ...others] = getAuthenticationArray(securitySchemes, input);
+  return others.reduce((prev, current) => EitherValidation.alt(prev, () => current), first);
 }
 
-function gatherValidationResults(
-  securitySchemes: HttpSecurityScheme[][],
-  input: Pick<IHttpRequest, 'headers' | 'url'>
-) {
-  const authResults = getAuthResults(securitySchemes, input);
-
-  const validSecurityScheme = authResults.find(authRes => authRes.every(Either.isRight));
-  const invalidSecuritySchemes = authResults.filter(authRes => authRes.some(Either.isLeft));
-
-  const firstLeft = invalidSecuritySchemes[0] && invalidSecuritySchemes[0].find(Either.isLeft);
-
-  if (!validSecurityScheme && firstLeft) {
-    return gatherInvalidResults(firstLeft, invalidSecuritySchemes);
-  } else {
-    return Option.none;
-  }
+function setErrorTag(authResults: NonEmptyArray<IPrismDiagnostic>) {
+  const tags = authResults.map(authResult => authResult.tags || []);
+  return set(['tags'], flatten(tags), authResults[0]);
 }
 
-function gatherWWWAuthHeader(
-  authResults: Array<Array<Either.Either<IPrismDiagnostic, unknown>>>,
-  pathToHeader: string[],
-  firstAuthErr: IPrismDiagnostic
-) {
-  const flattenedAuthResults = flatten(authResults);
-
-  if (flattenedAuthResults.length === 1) {
-    return firstAuthErr;
-  } else {
-    const wwwAuthenticateHeaders = flattenedAuthResults.map(authResult =>
-      pipe(
-        authResult,
-        Either.fold(result => result.tags || [], noop)
-      )
-    );
-
-    const firstAuthErrWithAuthHeader = set(pathToHeader, flatten(wwwAuthenticateHeaders), firstAuthErr);
-
-    return wwwAuthenticateHeaders.every(identity) ? firstAuthErrWithAuthHeader : firstAuthErr;
-  }
-}
-
-function getAuthResult(
-  firstAuthErrAsLeft: Either.Left<IPrismDiagnostic>,
-  authResult: Array<Either.Either<IPrismDiagnostic, unknown>>
-) {
-  const firstAuthErr = pipe(
-    firstAuthErrAsLeft,
-    Either.fold<IPrismDiagnostic, IPrismDiagnostic, IPrismDiagnostic>(identity, identity)
-  );
-
-  const invalidResultWithAuthHeader = gatherWWWAuthHeader([authResult], ['tags'], firstAuthErr);
-
-  return [Either.left(invalidResultWithAuthHeader)];
-}
-
-function getAuthResults(securitySchemes: HttpSecurityScheme[][], input: Pick<IHttpRequest, 'headers' | 'url'>) {
+function getAuthenticationArray(securitySchemes: HttpSecurityScheme[][], input: HeadersAndUrl) {
   return securitySchemes.map(securitySchemePairs => {
-    const authResult = securitySchemePairs.map(securityScheme =>
+    const authResults = securitySchemePairs.map(securityScheme =>
       pipe(
         findSecurityHandler(securityScheme),
-        Either.chain(f => f(input, 'name' in securityScheme ? securityScheme.name : ''))
+        Either.chain(securityHandler => securityHandler(input, 'name' in securityScheme ? securityScheme.name : '')),
+        Either.mapLeft<IPrismDiagnostic, NonEmptyArray<IPrismDiagnostic>>(e => [e])
       )
     );
 
-    const firstAuthErrAsLeft = authResult.find(Either.isLeft);
-
-    if (firstAuthErrAsLeft) {
-      return getAuthResult(firstAuthErrAsLeft, authResult);
-    } else {
-      return authResult;
-    }
+    return eitherSequence(authResults);
   });
 }
 
-export const validateSecurity: ValidatorFn<Pick<IHttpOperation, 'security'>, Pick<IHttpRequest, 'headers' | 'url'>> = ({
-  element,
-  resource,
-}) => {
-  const securitySchemes = resource.security;
-
-  if (!securitySchemes || !securitySchemes.length) {
-    return Either.right(element);
-  } else {
-    return pipe(
-      gatherValidationResults(securitySchemes, element),
-      Either.fromOption(() => element),
-      Either.swap,
-      Either.mapLeft<IPrismDiagnostic, NonEmptyArray<IPrismDiagnostic>>(e => [e])
-    );
-  }
-};
+export const validateSecurity: ValidatorFn<Pick<IHttpOperation, 'security'>, HeadersAndUrl> = ({ element, resource }) =>
+  pipe(
+    Option.fromNullable(resource.security),
+    Option.chain(Option.fromPredicate(isNonEmpty)),
+    Option.fold(
+      () => Either.right(element),
+      securitySchemes =>
+        pipe(
+          getValidationResults(securitySchemes, element),
+          Either.mapLeft<NonEmptyArray<IPrismDiagnostic>, NonEmptyArray<IPrismDiagnostic>>(e => [setErrorTag(e)]),
+          Either.map(() => element)
+        )
+    )
+  );
