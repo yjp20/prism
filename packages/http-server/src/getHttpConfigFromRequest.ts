5a3d4e841ce73f7786908bf8aa6bf1941c42aea0
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-11-20 17:23:06 -0600

    Use new Decoders (#1537)
    * use new decoding library
    
    * remove io-ts-types
    
    * mark as unknown instead of any
    
    * latest io-ts
    
    * import as lib for now

--- packages/http-server/src/getHttpConfigFromRequest.ts
+++ packages/http-server/src/getHttpConfigFromRequest.ts
@@ -1,28 +1,24 @@
 import { IHttpOperationConfig, IHttpRequest, ProblemJsonError, UNPROCESSABLE_ENTITY } from '@stoplight/prism-http';
 import { pipe } from 'fp-ts/pipeable';
 import * as E from 'fp-ts/Either';
-import * as t from 'io-ts';
-import { failure } from 'io-ts/lib/PathReporter';
-import { BooleanFromString } from 'io-ts-types/lib/BooleanFromString';
+import * as D from 'io-ts/lib/Decoder';
 //@ts-ignore
 import * as parsePreferHeader from 'parse-prefer-header';
 
-const PreferencesDecoder = t.union([
-  t.undefined,
-  t.partial(
-    {
-      code: t.string,
-      dynamic: t.string.pipe(BooleanFromString),
-      example: t.string,
-    },
-    'Preferences'
-  ),
-]);
+const BooleanFromString = D.parse<string, boolean>(s =>
+  s === 'true' ? D.success(true) : s === 'false' ? D.success(false) : D.failure(s, 'Not a boolean')
+);
+
+const PreferencesDecoder = D.partial({
+  code: D.string,
+  dynamic: pipe(D.string, BooleanFromString),
+  example: D.string,
+});
 
 type RequestPreferences = Partial<Omit<IHttpOperationConfig, 'mediaType'>>;
 
 export const getHttpConfigFromRequest = (req: IHttpRequest): E.Either<Error, RequestPreferences> => {
-  const preferences =
+  const preferences: unknown =
     req.headers && req.headers['prefer']
       ? parsePreferHeader(req.headers['prefer'])
       : { code: req.url.query?.__code, dynamic: req.url.query?.__dynamic, example: req.url.query?.__example };
@@ -30,7 +26,7 @@ export const getHttpConfigFromRequest = (req: IHttpRequest): E.Either<Error, Req
   return pipe(
     PreferencesDecoder.decode(preferences),
     E.bimap(
-      err => ProblemJsonError.fromTemplate(UNPROCESSABLE_ENTITY, failure(err).join('; ')),
+      err => ProblemJsonError.fromTemplate(UNPROCESSABLE_ENTITY, D.draw(err)),
       parsed => ({ code: parsed?.code, exampleKey: parsed?.example, dynamic: parsed?.dynamic })
     )
   );
