5af15fdac6576db4a0955f0fdc5f9f21830e9ae0
Vincenzo Chianese <vincenz.chianese@icloud.com>
2021-01-07 15:26:22 +0100

    use else to stop evaluation (#1623)

--- packages/http/src/mocker/generator/HttpParamGenerator.ts
+++ packages/http/src/mocker/generator/HttpParamGenerator.ts
@@ -17,25 +17,15 @@ export function improveSchema(schema: JSONSchema): JSONSchema {
     }
   }
 
-  if (newSchema.type === 'string') {
-    if (!newSchema.format && !newSchema.enum && !newSchema.pattern) {
-      newSchema['x-faker'] = 'lorem.word';
-    }
-  }
-
-  if (newSchema.type === 'object') {
-    if (newSchema.properties) {
-      newSchema.properties = Object.entries(newSchema.properties).reduce((r, [k, v]) => {
-        r[k] = improveSchema(v);
-        return r;
-      }, {});
-    }
-  }
-
-  if (newSchema.type === 'array') {
-    if (typeof newSchema.items === 'object') {
-      newSchema.items = improveSchema(newSchema.items);
-    }
+  if (newSchema.type === 'string' && !newSchema.format && !newSchema.enum && !newSchema.pattern) {
+    newSchema['x-faker'] = 'lorem.word';
+  } else if (newSchema.type === 'object' && newSchema.properties) {
+    newSchema.properties = Object.entries(newSchema.properties).reduce((r, [k, v]) => {
+      r[k] = improveSchema(v);
+      return r;
+    }, {});
+  } else if (newSchema.type === 'array' && typeof newSchema.items === 'object') {
+    newSchema.items = improveSchema(newSchema.items);
   }
 
   return newSchema;
