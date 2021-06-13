33516974d1bceae8ddb3a0a5c72a40f7d282f69b
Jake Son <sonjeabin@naver.com>
2021-06-13 16:49:30 +0900

    change deprecated usage of fp-ts instances

--- packages/cli/src/combinators.ts
+++ packages/cli/src/combinators.ts
@@ -2,5 +2,5 @@ import * as E from 'fp-ts/Either';
 import * as A from 'fp-ts/Array';
 import { sequenceS } from 'fp-ts/Apply';
 
-export const traverseEither = A.array.traverse(E.either);
-export const sequenceSEither = sequenceS(E.either);
+export const traverseEither = A.traverse(E.Applicative);
+export const sequenceSEither = sequenceS(E.Apply);
